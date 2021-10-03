5fdea47a088deff6d8768d916385a20b3c2fa146
Bartlomiej <bartlomiejswiech99@gmail.com>
2021-10-03 23:20:27 +0200

    Implement localStorage and all functional buttons. Suble changes in css. Bug fixes.

--- static/scripts/todo.js
+++ static/scripts/todo.js
@@ -1,13 +1,29 @@
-import addGlobalListener from './globalListener.js';
+import addGlobalListener, {
+    swap
+} from './utilites.js';
 
 export default class ToDo {
     constructor() {
+        this.todoLocalStorage = null;
         this.todoList = document.getElementById('todo__list');
+        this.todoPrint = document.getElementById('todo__print');
+        this.todoExport = document.getElementById('todo__export');
+        this.todoImport = document.getElementById('todo__import');
+        this.todoToolbar = document.getElementById('todo__toolbar');
+        this.todoFooter = document.getElementById('todo__footer');
+        this.showTodoTasks = document.getElementById('todo__tasks');
+        this.showTodoTasksRemoved = document.getElementById('todo__tasks-removed');
+        this.showTodoTasksCompleted = document.getElementById('todo__tasks-completed');
+        this.showTodoTasksReset = document.getElementById('todo__tasks-reset');
+        this.showTodoCalendar = document.getElementById('todo__calendar');
+        this.todoTasks = document.getElementById('todo__tasks');
         this.todoCheckAll = document.getElementById('todo__check-all');
         this.todoUncheckAll = document.getElementById('todo__uncheck-all');
         this.todoRemoveAll = document.getElementById('todo__remove-all');
         this.todoCompleteAll = document.getElementById('todo__complete-all');
         this.todoAddItem = document.getElementById('todo__item-add');
+
+        this.loadLocalStorage();
 
         this.initEvents();
     }
@@ -21,29 +37,133 @@ export default class ToDo {
         addGlobalListener('click', '.todo__item-move-up', this.moveUpItem);
 
         // regular listeners for statically added elements
+        this.showTodoTasks.addEventListener('click', this.displayTodoList);
+        this.showTodoTasksRemoved.addEventListener('click', () => {
+            this.displayTodoList('todoRemovedTasks');
+        });
+        this.showTodoTasksCompleted.addEventListener('click', () => {
+            this.displayTodoList('todoCompletedTasks');
+        });
+        this.showTodoTasksReset.addEventListener('click', this.resetTodoList);
+
         this.todoCheckAll.addEventListener('click', this.checkAll);
         this.todoUncheckAll.addEventListener('click', this.uncheckAll);
         this.todoAddItem.addEventListener('click', this.addItem);
         this.todoRemoveAll.addEventListener('click', this.removeAllItems);
         this.todoCompleteAll.addEventListener('click', this.completeAllItems);
+        this.todoPrint.addEventListener('click', this.printList);
+        this.todoExport.addEventListener('click', this.exportList);
+        this.todoImport.addEventListener('click', this.importList);
+    }
+
+    loadLocalStorage = () => {
+        this.todoLocalStorage = JSON.parse(localStorage.getItem('todo'));
+
+        if (this.todoLocalStorage === null) {
+            this.todoLocalStorage = {}
+            this.todoLocalStorage.todoTasks = [];
+            this.todoLocalStorage.todoRemovedTasks = [];
+            this.todoLocalStorage.todoCompletedTasks = [];
+        }
+
+        this.displayTodoList();
+    }
+
+    displayTodoList = (_type = "todoTasks") => {
+        this.todoList.innerHTML = "";
+        switch (_type) {
+            case 'todoRemovedTasks':
+                this.todoToolbar.style.display = "none";
+                this.todoFooter.style.display = "none";
+
+                if (this.todoLocalStorage.todoRemovedTasks.length) {
+                    this.todoLocalStorage.todoRemovedTasks.forEach((_text) => {
+                        this.displayItem(_text);
+                    });
+                } else {
+                    this.displayErrorMessage();
+                }
+                break;
+            case 'todoCompletedTasks':
+                this.todoToolbar.style.display = "none";
+                this.todoFooter.style.display = "none";
+
+                if (this.todoLocalStorage.todoCompletedTasks.length) {
+                    this.todoLocalStorage.todoCompletedTasks.forEach((_text) => {
+                        this.displayItem(_text);
+                    });
+                } else {
+                    this.displayErrorMessage();
+                }
+
+                break;
+            default:
+                this.todoToolbar.style.display = "flex";
+                this.todoFooter.style.display = "flex";
+
+                if (this.todoLocalStorage.todoTasks.length) {
+                    this.todoLocalStorage.todoTasks.forEach((_text) => {
+                        this.displayItem(_text, true);
+                    });
+                } else {
+                    this.displayErrorMessage();
+                }
+                break;
+        }
+    }
+
+    resetTodoList = () => {
+        this.todoLocalStorage = {};
+        this.todoLocalStorage.todoTasks = [];
+        this.todoLocalStorage.todoRemovedTasks = [];
+        this.todoLocalStorage.todoCompletedTasks = [];
+        localStorage.setItem('todo', JSON.stringify(this.todoLocalStorage));
+        this.displayTodoList();
     }
 
     toggleCheck = (e) => {
-        e.target.closest('.todo__item').classList.toggle('todo__item--checked');
+        const item = e.target.closest('.todo__item');
+
+        item.classList.toggle('todo__item--checked');
     }
 
     removeItem = (e) => {
-        e.target.closest('.todo__item').remove();
+        const item = e.target.closest('.todo__item');
+        const itemText = item.querySelector('.todo__item-text').textContent;
+
+        this.todoLocalStorage.todoTasks.splice([...item.parentNode.children].indexOf(item), 1);
+        this.todoLocalStorage.todoRemovedTasks.push(itemText);
+        localStorage.setItem('todo', JSON.stringify(this.todoLocalStorage));
+
+        item.remove();
+
+        if (this.todoLocalStorage.todoTasks.length === 0) {
+            this.displayErrorMessage();
+        }
     }
 
     completeItem = (e) => {
-        e.target.closest('.todo__item').remove();
+        const item = e.target.closest('.todo__item');
+        const itemText = item.querySelector('.todo__item-text').textContent;
+
+        this.todoLocalStorage.todoTasks.splice([...item.parentNode.children].indexOf(item), 1);
+        this.todoLocalStorage.todoCompletedTasks.push(itemText);
+        localStorage.setItem('todo', JSON.stringify(this.todoLocalStorage));
+
+        item.remove();
+
+        if (this.todoLocalStorage.todoTasks.length === 0) {
+            this.displayErrorMessage();
+        }
     }
 
     moveDownItem = (e) => {
         const item = e.target.closest('.todo__item');
 
         if (item.nextElementSibling === null) return;
+
+        this.todoLocalStorage.todoTasks = swap(this.todoLocalStorage.todoTasks, [...item.parentNode.children].indexOf(item), [...item.parentNode.children].indexOf(item.nextElementSibling));
+        localStorage.setItem('todo', JSON.stringify(this.todoLocalStorage));
 
         item.parentNode.insertBefore(item.nextElementSibling, item);
     }
@@ -53,56 +173,73 @@ export default class ToDo {
 
         if (item.previousElementSibling === null) return;
 
+        this.todoLocalStorage.todoTasks = swap(this.todoLocalStorage.todoTasks, [...item.parentNode.children].indexOf(item), [...item.parentNode.children].indexOf(item.previousElementSibling));
+        localStorage.setItem('todo', JSON.stringify(this.todoLocalStorage));
+
         item.parentNode.insertBefore(item, item.previousElementSibling);
     }
 
     checkAll = () => {
-        document.querySelectorAll('.todo__item').forEach((item) => {
-            item.classList.add('todo__item--checked');
+        document.querySelectorAll('.todo__item').forEach((_item) => {
+            _item.classList.add('todo__item--checked');
         });
     }
 
     uncheckAll = () => {
-        document.querySelectorAll('.todo__item').forEach((item) => {
-            item.classList.remove('todo__item--checked');
+        document.querySelectorAll('.todo__item').forEach((_item) => {
+            _item.classList.remove('todo__item--checked');
         });
     }
 
     removeAllItems = () => {
-        document.querySelectorAll('.todo__item--checked').forEach((item) => {
-            item.remove();
-        });
-    }
+        document.querySelectorAll('.todo__item--checked').forEach((_item) => {
+            const itemText = _item.querySelector('.todo__item-text').textContent;
 
-    removeAllItems = () => {
-        document.querySelectorAll('.todo__item--checked').forEach((item) => {
-            item.remove();
+            this.todoLocalStorage.todoTasks.splice([..._item.parentNode.children].indexOf(_item), 1);
+            this.todoLocalStorage.todoRemovedTasks.push(itemText);
+            localStorage.setItem('todo', JSON.stringify(this.todoLocalStorage));
+
+            _item.remove();
         });
+
+        if (this.todoLocalStorage.todoTasks.length === 0) {
+            this.displayErrorMessage();
+        }
     }
 
     completeAllItems = () => {
-        document.querySelectorAll('.todo__item--checked').forEach((item) => {
-            item.remove();
+        document.querySelectorAll('.todo__item--checked').forEach((_item) => {
+            const itemText = _item.querySelector('.todo__item-text').textContent;
+
+            this.todoLocalStorage.todoTasks.splice([..._item.parentNode.children].indexOf(_item), 1);
+            this.todoLocalStorage.todoCompletedTasks.push(itemText);
+            localStorage.setItem('todo', JSON.stringify(this.todoLocalStorage));
+
+            _item.remove();
         });
+
+
+        if (this.todoLocalStorage.todoTasks.length === 0) {
+            this.displayErrorMessage();
+        }
     }
 
-    addItem = (e) => {
+    displayItem = (_text, _showActions = false) => {
         const newItem = document.createElement('div');
-        const newItemText = prompt("Your new todo element:");
-
-        if (newItemText == null) {
-            return;
-        } else if (newItemText.length < 3) {
-            alert(`Too short! Item not created!`);
-            return;
-        }
+        let newItemContent = "";
 
         newItem.classList.add('todo__item');
-        newItem.innerHTML = `<div class="todo__item-content">
-        <div class="todo__item-check"></div>
-        <span>${newItemText}</span>
-        </div>
-        <div class="todo__item-actions">
+        newItemContent = `<div class="todo__item-content">`
+
+        if (_showActions) {
+            newItemContent += `<div class="todo__item-check"></div>`
+        }
+
+        newItemContent += `<span class="todo__item-text">${_text}</span>
+            </div>`
+
+        if (_showActions) {
+            newItemContent += `<div class="todo__item-actions">
             <div class="todo__item-btn todo__item-remove">
                 <i class="fas fa-trash"></i>
             </div>
@@ -114,9 +251,86 @@ export default class ToDo {
             </div>
             <div class="todo__item-btn todo__item-move-up">
                 <i class="fas fa-arrow-up"></i>
-            </div>
-        </div>`;
+            </div>`
+        }
+
+        newItemContent += `</div>`;
+
+        newItem.innerHTML = newItemContent;
 
         this.todoList.append(newItem);
+    }
+
+    addItem = (e, _text = null) => {
+        let newItemText = null;
+
+        newItemText = prompt("Your new todo element:");
+        if (newItemText == null) {
+            return;
+        } else if (newItemText.length < 3) {
+            alert(`Too short! Item not created!`);
+            return;
+        }
+
+        if (this.todoLocalStorage.todoTasks.length === 0) {
+            this.todoList.innerHTML = "";
+        }
+
+        this.todoLocalStorage.todoTasks.push(newItemText);
+        localStorage.setItem('todo', JSON.stringify(this.todoLocalStorage));
+        this.displayItem(newItemText, true);
+    }
+
+    printList = () => {
+        this.displayTodoList();
+        window.print();
+    }
+
+    exportList = () => {
+        const file = document.createElement('a');
+        file.setAttribute('download', 'todo.txt');
+        file.href = window.URL.createObjectURL(new Blob([JSON.stringify(this.todoLocalStorage)], {
+            type: 'text/csv'
+        }));
+
+        const evt = new MouseEvent('click');
+
+        file.dispatchEvent(evt);
+    }
+
+    importList = () => {
+        const fileReader = new FileReader;
+        const inputFile = document.createElement('input');
+        inputFile.setAttribute('type', 'file');
+
+        inputFile.addEventListener('change', () => {
+            let files = inputFile.files;
+
+            if (files.length === 0) return;
+
+            const file = files[0];
+
+            const reader = new FileReader();
+
+            reader.onload = (e) => {
+                const file = e.target.result;
+
+                this.todoLocalStorage = JSON.parse(file);
+                localStorage.setItem('todo', JSON.stringify(this.todoLocalStorage));
+                this.displayTodoList();
+            }
+
+            reader.onerror = (e) => alert('error!');
+
+            reader.readAsText(file);
+        })
+
+        const evt = new MouseEvent('click');
+
+        inputFile.dispatchEvent(evt);
+    }
+
+    displayErrorMessage = () => {
+        this.todoList.innerHTML = `<div class="message">No tasks available!</div>`;
     }
 }
--- static/scripts/utilites.js
+++ static/scripts/utilites.js
@@ -5,3 +5,12 @@ export default function addGlobalListener(_event, _selector, _callback) {
         }
     })
 }
+
+export function swap(_array, _index1, _index2) {
+    let buffer = _array[_index1];
+
+    _array[_index1] = _array[_index2];
+    _array[_index2] = buffer;
+
+    return _array;
+}
